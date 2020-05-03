b53cca20721a83c12c2de821bc2fc45d1c088b4d
ErikFox <erikmichaelfox@gmail.com>
2020-05-03 18:52:36 -0400

    Add completed project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'discountCalculator';
+  price: any;
+
+  discount: any;
+
+  priceAfterDiscount: any;
+
+  discountCalc(): any {
+    var actualDiscount = (this.discount / 100) * this.price;
+    return  this.priceAfterDiscount = "$" + (Math.round((this.price - actualDiscount) * 100) / 100).toFixed(2);
+  }
+
 }
